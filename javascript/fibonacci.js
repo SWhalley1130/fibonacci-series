bf8eee9f76d3d859fd9a0871a676f17934cd6315
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-01 16:41:33 -0700

    Completes lab

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-  // type your code here
+
+  //Begin array with starter values
+  let newArray=[0,1];
+
+  //Start a loop to add previous two index values and assign the value to the current index
+  //The loop starts at the 2 index because the array already has values at 0 and 1
+  //The loop continues to run until we've hit our target number
+  for (let i=2; i<=num; i++)
+  {
+    newArray[i]=newArray[i-2]+newArray[i-1];
+  }
+ 
+  //Return the value of the array at the given number
+  return (newArray[num]);
 }
 
 if (require.main === module) {
